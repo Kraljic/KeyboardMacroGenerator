b2efd9e50ade6878098d1c7246f404dc6444d479
kraljic <marko.kraljic10@gmail.com>
2022-08-06 00:45:42 +0200

    Added new macro mappings

--- src/MacroMappings.ts
+++ src/MacroMappings.ts
@@ -11,46 +11,75 @@ const mvnCleanPackage = mvnMacro("mvn clean package");
 
 const intelliJ_build                    = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_F9);
 const intelliJ_run                      = keyPress(HID_KEYS.KEY_MOD_LSHIFT, HID_KEYS.KEY_F10);
+const intelliJ_stop                     = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_F2);
 const intelliJ_debug                    = keyPress(HID_KEYS.KEY_MOD_LSHIFT, HID_KEYS.KEY_F9);
 const intelliJ_debug_breakpoint         = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_F8);
-const intelliJ_debug_muteBreakpoint     = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_MOD_LSHIFT, HID_KEYS.KEY_F8);
+const intelliJ_debug_muteBreakpoints    = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_MOD_LALT, HID_KEYS.KEY_F10);
+const intelliJ_debug_showBreakpoints    = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_MOD_LSHIFT, HID_KEYS.KEY_F8);
 const intelliJ_debug_stepover           = keyPress(HID_KEYS.KEY_F8);
 const intelliJ_debug_stepinto           = keyPress(HID_KEYS.KEY_F7);
 const intelliJ_debug_stepout            = keyPress(HID_KEYS.KEY_MOD_LSHIFT, HID_KEYS.KEY_F8);
 const intelliJ_debug_runToCursor        = keyPress(HID_KEYS.KEY_MOD_LALT, HID_KEYS.KEY_F9);
 const intelliJ_debug_resume             = keyPress(HID_KEYS.KEY_F9);
-const intelliJ_debug_stop               = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_F2);
 const intelliJ_debug_evalExpDialog      = keyPress(HID_KEYS.KEY_MOD_LALT, HID_KEYS.KEY_F8);
 const intelliJ_debug_evalExpNow         = keyPress(HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_MOD_LALT, HID_KEYS.KEY_F8);
 
 export const MACRO_MAP: MacroMap[] = [
     {
         macro: intelliJ_build,
-        keyCombination: [HID_KEYS.KEY_MOD_LCTRL, HID_KEYS.KEY_Q],
+        keyCombination: [HID_KEYS.KEY_KPDOT],
     },
     {
         macro: intelliJ_run,
-        keyCombination: [HID_KEYS.KEY_INSERT],
+        keyCombination: [HID_KEYS.KEY_KPENTER],
     },
     {
         macro: intelliJ_debug,
-        keyCombination: [HID_KEYS.KEY_HOME],
+        keyCombination: [HID_KEYS.KEY_KPPLUS],
+    },
+    {
+        macro: intelliJ_stop,
+        keyCombination: [HID_KEYS.KEY_KPMINUS],
+    },
+    {
+        macro: mvnCleanPackage,
+        keyCombination: [HID_KEYS.KEY_KP0],
     },
     {
         macro: intelliJ_debug_breakpoint,
-        keyCombination: [HID_KEYS.KEY_PAGEUP],
+        keyCombination: [HID_KEYS.KEY_NUMLOCK],
+    },
+    {
+        macro: intelliJ_debug_muteBreakpoints,
+        keyCombination: [HID_KEYS.KEY_KPSLASH],
+    },
+    {
+        macro: intelliJ_debug_showBreakpoints,
+        keyCombination: [HID_KEYS.KEY_KPASTERISK],
     },
     {
         macro: intelliJ_debug_stepover,
-        keyCombination: [HID_KEYS.KEY_DELETE],
+        keyCombination: [HID_KEYS.KEY_KP7],
     },
     {
         macro: intelliJ_debug_stepinto,
-        keyCombination: [HID_KEYS.KEY_END],
+        keyCombination: [HID_KEYS.KEY_KP8],
     },
     {
         macro: intelliJ_debug_stepout,
-        keyCombination: [HID_KEYS.KEY_PAGEDOWN],
+        keyCombination: [HID_KEYS.KEY_KP9],
+    },
+    {
+        macro: intelliJ_debug_runToCursor,
+        keyCombination: [HID_KEYS.KEY_KP4],
+    },
+    {
+        macro: intelliJ_debug_evalExpDialog,
+        keyCombination: [HID_KEYS.KEY_KP5],
+    },
+    {
+        macro: intelliJ_debug_resume,
+        keyCombination: [HID_KEYS.KEY_KP6],
     },
 ];
 
